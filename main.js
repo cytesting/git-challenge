466878299633c8cea206d7260bc62289eb0d2830
leomartinez2020 <lowenhard@yahoo.com>
2022-03-21 15:10:58 -0500

    Agregar canvas al HTML

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello javascript');
+function draw() {
+  const canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    const width = 100;
+    const height = 100;
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(width, width, height, height);
+  }
+}
+
+draw();
+
+console.log('Animating...');
